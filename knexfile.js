74b90fd650e107f22c9c120c7207885e44e63954
leonardosfonseca <75985026+leonardosfonseca@users.noreply.github.com>
2021-04-11 20:22:59 -0300

    Add files via upload

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   test: {
     client: 'pg',
-    version: '13.2',
     connection: {
       host: 'db',
       user: 'postgres',
       password: '123456',
-      database: 'developers',
+      database: 'teste',
     },
     migrations: {
       directory: 'src/migrations',
